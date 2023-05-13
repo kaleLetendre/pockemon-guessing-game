16b87f9890075dd35d17355a5bd331ce0e43c63b
Nabil M. Al-Rousan <rousani@gmail.com>
2023-05-12 23:27:10 -0700

    Step 5. Check if two cards are the same.

--- index.js
+++ index.js
@@ -2,8 +2,25 @@
 
 
 const setup = () => {
+  let firstCard = undefined
+  let secondCard = undefined
   $(".card").on(("click"), function () {
     $(this).toggleClass("flip");
+
+    if (!firstCard)
+      firstCard = $(this).find(".front_face")[0]
+    else {
+      secondCard = $(this).find(".front_face")[0]
+      console.log(firstCard, secondCard);
+      if (
+        firstCard.src
+        ==
+        secondCard.src
+      )
+        console.log("match")
+      else
+        console.log("no match")
+    }
   });
 }
 
--- tutorial.md
+++ tutorial.md
@@ -4,3 +4,4 @@
 # Step 4. Add a flip animation to the cards on hover.
 # Step 4.1 Add the flip class to rotateY(180deg) to the .card:hover and transition in 1s
 # Step 4.2 Make the rotation in 3d by adding perspective: 1000px; to the .card
+# Step 5. Check if two cards are the same.
